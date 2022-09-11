9e89737e84f1bd3bdfe0bb2b418237195c47ead9
Vadim Kostrov <vmkvo15@outlook.com>
2022-09-11 15:26:04 +0400

    Create skeleton render function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const GAMEBOARD = (function() {
-    gameboard: {null, null, null, null, null, null, null, null, null}
-    render: () => {
+    let gameboard = ['x', 'o', null, null, null, null, null, null, null];
 
-    }
+    function render() {
+        let canvas = document.querySelector('.container');
+        let boxWidth = 500 / 3;
+        let boxHeight = 500 / 3;
+
+        gameboard.forEach(function(square) {
+            let box = document.createElement('div');
+            if (square === 'x') {
+                box.textContent = 'X';
+                box.style.width = boxWidth + 'px';
+                box.style.height = boxHeight + 'px';
+                canvas.appendChild(box);
+            } else if (square === 'o') {
+                box.textContent = 'O';
+                box.style.width = boxWidth + 'px';
+                box.style.height = boxHeight + 'px';
+                canvas.appendChild(box);
+            }
+        })
+    };
+
+    render();
+
 })();
 
