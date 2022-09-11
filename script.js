eba63d47067b2d36ce5992ff79af0ad6d9659231
Vadim Kostrov <vmkvo15@outlook.com>
2022-09-11 16:20:00 +0400

    Make render function fully dynamic

--- script.js
+++ script.js
@@ -4,28 +4,33 @@ const GAMEBOARD = (function() {
     function render() {
         let container = document.querySelector('.container');
         let canvas = document.querySelector('.canvas');
-        let boxWidth = container.offsetWidth / 3;
-        let boxHeight = container.offsetHeight / 3;
-
+        
         gameboard.forEach(function(square) {
             createBox(square);
-        })
+        });
 
         function createBox(value) {
             let box = document.createElement('div');
+            let boxWidth = container.offsetWidth / 3;
+            let boxHeight = container.offsetHeight / 3;
+            
+
             if (value === 'x') {
                 box.textContent = 'X';
-                box.style.width = boxWidth + 'px';
-                box.style.height = boxHeight + 'px';
+                box.style.width = '1fr';
+                box.style.height = '1fr';
+                box.className = 'box';
                 canvas.appendChild(box);
             } else if (value === 'o') {
                 box.textContent = 'O';
-                box.style.width = boxWidth + 'px';
-                box.style.height = boxHeight + 'px';
+                box.style.width = '1fr';
+                box.style.height = '1fr';
+                box.className ='box';
                 canvas.appendChild(box);
             } else {
-                box.style.width = boxWidth + 'px';
-                box.style.height = boxHeight + 'px';
+                box.style.width = '1fr';
+                box.style.height = '1fr';
+                box.className = 'box';
                 canvas.appendChild(box);
             }
         }
