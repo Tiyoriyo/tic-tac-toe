be4d4aab7710cbf3b45e51e5e9e81b92783ceae4
Vadim Kostrov <vmkvo15@outlook.com>
2022-09-11 15:55:47 +0400

    Make it so render function is semi-dynamic

--- script.js
+++ script.js
@@ -2,24 +2,33 @@ const GAMEBOARD = (function() {
     let gameboard = ['x', 'o', null, null, null, null, null, null, null];
 
     function render() {
-        let canvas = document.querySelector('.container');
-        let boxWidth = 500 / 3;
-        let boxHeight = 500 / 3;
+        let container = document.querySelector('.container');
+        let canvas = document.querySelector('.canvas');
+        let boxWidth = container.offsetWidth / 3;
+        let boxHeight = container.offsetHeight / 3;
 
         gameboard.forEach(function(square) {
+            createBox(square);
+        })
+
+        function createBox(value) {
             let box = document.createElement('div');
-            if (square === 'x') {
+            if (value === 'x') {
                 box.textContent = 'X';
                 box.style.width = boxWidth + 'px';
                 box.style.height = boxHeight + 'px';
                 canvas.appendChild(box);
-            } else if (square === 'o') {
+            } else if (value === 'o') {
                 box.textContent = 'O';
                 box.style.width = boxWidth + 'px';
                 box.style.height = boxHeight + 'px';
                 canvas.appendChild(box);
+            } else {
+                box.style.width = boxWidth + 'px';
+                box.style.height = boxHeight + 'px';
+                canvas.appendChild(box);
             }
-        })
+        }
     };
 
     render();
