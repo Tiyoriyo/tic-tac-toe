c24cf864b5b6683fa14c5185571e031f505b04d8
Vadim Kostrov <vmkvo15@outlook.com>
2022-09-16 14:16:17 +0400

    Add intro elements automatically via JS

--- script.js
+++ script.js
@@ -14,6 +14,39 @@ const GAMEBOARD = (function() {
         [2, 4, 6]
     ];
 
+    function intro() {
+        (function createIntroItems() {
+            const GAMETITLE = document.createElement('h1');
+            GAMETITLE.textContent = 'Tic-Tac-Toe'
+            GAMETITLE.className = 'game-title';
+
+            const BUTTONSET = document.createElement('div');
+            BUTTONSET.className = 'btn-set';
+
+            const TWOPLAYER = document.createElement('button');
+            TWOPLAYER.textContent = 'Two Player';
+            TWOPLAYER.className = 'intro-btn two-player-btn';
+            TWOPLAYER.addEventListener('click', () => {
+
+            });
+            BUTTONSET.appendChild(TWOPLAYER);
+
+            const COMPUTER = document.createElement('button');
+            COMPUTER.textContent = 'Computer';
+            COMPUTER.className = 'intro-btn computer-btn';
+            COMPUTER.addEventListener('click', () => {
+
+            });
+            BUTTONSET.appendChild(COMPUTER);
+
+            const MAINCONTAINER = document.querySelector('.main-container');
+            MAINCONTAINER.appendChild(GAMETITLE);
+            MAINCONTAINER.appendChild(BUTTONSET);
+        })();
+    }
+
+    intro();
+
     function render() {
         let canvas = document.querySelector('.canvas');
         let i = 0;
