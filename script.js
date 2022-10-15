ce7b1b3031a11e0a4144d82fd88c6073865300f7
Vadim Kostrov <vmkvo15@outlook.com>
2022-10-15 16:44:32 +0400

    Reorganize addPlayerInputs code

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const intro = (() => {
     const MAINCONTAINER = document.querySelector('.main-container');
 
     const createIntroItems = () => {
-
         MAINCONTAINER.innerHTML += `<h1 class="game-title">Tic-Tac-Toe</h1>
         <div class="button-set"><button class="intro-btn two-player-btn">Two Player</button>
         <button class="intro-btn computer-btn">Computer</button>
@@ -25,121 +24,41 @@ const intro = (() => {
     createIntroItems();
 
     const addPlayerInputs = () => {
-        // INPUTS UNDERNEATH 
-        // ------ INPUT CONTAINER ------
-        const INPUTSET = document.createElement('div');
-        INPUTSET.style.display = 'flex';
+        MAINCONTAINER.innerHTML += `<div style="display: flex;">
+        <div style="display: flex; flex-direction: column;">
+        <label>Player 1 Name</label>
+        <input id="P1_input">
+        <div style="display: flex;">
+        <p>X</p>
+        <input type="radio" value="X" name="P1Choice" class="P1Choice">
+        <p>O</p>
+        <input type="radio" value="O" name="P1Choice" class="P1Choice">
+        </div>
+        </div>
+        <div style="display: flex; flex-direction: column;">
+        <label>Player 2 Name</label>
+        <input id="P2_input">
+        <div style="display: flex;">
+        <p>X</p>
+        <input type="radio" value="X" name="P2Choice" class="P2Choice">
+        <p>O</p>
+        <input type="radio" value="O" name="P2Choice" class="P2Choice">
+        </div>
+        </div>
+        </div>
+        <div style="display: flex;">
+        <button class="confirm-btn">Confirm</button>
+        <button class="back-btn">Back</button>
+        </div>
+        </div>`
 
-        // ---- PLAYER INPUT CONTAINERS ----
-        const P1DIV = document.createElement('div');
-        P1DIV.style.display = 'flex';
-        P1DIV.style.flexDirection = 'column';
-        const P2DIV = document.createElement('div');
-        P2DIV.style.display = 'flex';
-        P2DIV.style.flexDirection = 'column';
+        const Confirm = document.querySelector('.confirm-btn');
+        const Back = document.querySelector('.back-btn');
 
-        // -- PLAYER 1 INPUT & LABEL
-        const P1LABEL = document.createElement('label');
-        P1LABEL.textContent = 'Player 1 Name'
-        const P1INPUT = document.createElement('input');
-        P1INPUT.id = 'P1_input';
-        P1DIV.appendChild(P1LABEL);
-        P1DIV.appendChild(P1INPUT);
-
-        // -- PLAYER 1 TEAM CHOICE
-        const P1CHOICESET = document.createElement('div');
-        P1CHOICESET.style.display = 'flex';
-        
-        const P1XCHOICE_LABEL = document.createElement('p');
-        P1XCHOICE_LABEL.textContent = 'X';
-
-
-        const P1XCHOICE = document.createElement('input');
-        P1XCHOICE.type = 'radio';
-        P1XCHOICE.value = 'X';
-        P1XCHOICE.name = 'P1Choice'
-        P1XCHOICE.classList = 'P1Choice';
-
-        const P1OCHOICE_LABEL = document.createElement('p');
-        P1OCHOICE_LABEL.textContent = 'O';
-
-        const P1OCHOICE = document.createElement('input');
-        P1OCHOICE.type = 'radio';
-        P1OCHOICE.value = 'O';
-        P1OCHOICE.name = 'P1Choice';
-        P1OCHOICE.classList = 'P1Choice';
-        
-
-        P1CHOICESET.appendChild(P1XCHOICE_LABEL);
-        P1CHOICESET.appendChild(P1XCHOICE);
-        P1CHOICESET.appendChild(P1OCHOICE_LABEL);
-        P1CHOICESET.appendChild(P1OCHOICE);
-
-        // -- PLAYER 1 APPEND RADIO BUTTONS & LABELS TO MAIN P1 DIV
-        P1DIV.appendChild(P1CHOICESET);
-
-        // -- PLAYER 2 INPUT & LABEL
-        const P2LABEL = document.createElement('label');
-        P2LABEL.textContent = 'Player 2 Name';
-        const P2INPUT = document.createElement('input');
-        P2INPUT.id = 'P2_input'
-        P2DIV.appendChild(P2LABEL);
-        P2DIV.appendChild(P2INPUT);
-
-        // -- PLAYER 2 TEAM CHOICE
-        const P2CHOICESET = document.createElement('div');
-        P2CHOICESET.style.display = 'flex';
-        
-        const P2XCHOICE_LABEL = document.createElement('p');
-        P2XCHOICE_LABEL.textContent = 'X';
+        Confirm.addEventListener('click', dummyFuncConfirm);
+        Back.addEventListener('click', dummyFuncBack);
 
 
-        const P2XCHOICE = document.createElement('input');
-        P2XCHOICE.type = 'radio';
-        P2XCHOICE.value = 'X';
-        P2XCHOICE.name = 'P2Choice'
-        P2XCHOICE.className = 'P2Choice';
-
-        const P2OCHOICE_LABEL = document.createElement('p');
-        P2OCHOICE_LABEL.textContent = 'O';
-
-        const P2OCHOICE = document.createElement('input');
-        P2OCHOICE.type = 'radio';
-        P2OCHOICE.value = 'O';
-        P2OCHOICE.name = 'P2Choice';
-        P2OCHOICE.className = 'P2Choice';
-        
-
-        P2CHOICESET.appendChild(P2XCHOICE_LABEL);
-        P2CHOICESET.appendChild(P2XCHOICE);
-        P2CHOICESET.appendChild(P2OCHOICE_LABEL);
-        P2CHOICESET.appendChild(P2OCHOICE);
-
-        // -- PLAYER 2 APPEND RADIO BUTTONS & LABELS TO MAIN P2 DIV
-        P2DIV.appendChild(P2CHOICESET);
-
-        // APPEND P1 & P2 DIVS TO INPUT CONTAINER
-        INPUTSET.appendChild(P1DIV);
-        INPUTSET.appendChild(P2DIV);
-        MAINCONTAINER.appendChild(INPUTSET);
-
-        // BUTTONS UNDERNEATH ------------
-        const BUTTONSET = document.createElement('div');
-        BUTTONSET.style.display = 'flex';
-
-        const CONFIRM = document.createElement('button');
-        CONFIRM.textContent = 'Confirm';
-        CONFIRM.addEventListener('click', dummyFuncConfirm);
-
-        const BACK = document.createElement('button');
-        BACK.textContent = 'Back';
-        BACK.addEventListener('click', dummyFuncBack);
-
-        
-
-        BUTTONSET.appendChild(CONFIRM);
-        BUTTONSET.appendChild(BACK);
-        MAINCONTAINER.appendChild(BUTTONSET);
 
     }   
 
